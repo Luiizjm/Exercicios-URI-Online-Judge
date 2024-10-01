dc2082f83e38372575f3d1c664dede130afc1f64
Luiizjm <luiizjm@outlook.com>
2024-10-01 17:09:57 -0300

    Exercicios URI

---
+++ ExerciciosURI/URI1019/URI1019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int horas, minutos, segundos;
+        int resto_horas;
+
+        Console.Write("Segundos: ");
+        int entrada_seg = int.Parse(Console.ReadLine());
+
+        horas = entrada_seg / 3600;
+        resto_horas = entrada_seg % 3600;
+
+        minutos = resto_horas / 60;
+        segundos = resto_horas % 60;
+
+        Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+    }
+
+}
