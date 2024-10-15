be634358c628d04ac334e3254c6a285c5f6c7b4f
Luiizjm <luiizjm@outlook.com>
2024-10-15 20:44:19 -0300

    .

---
+++ ExerciciosURI/URI1061/URI1061/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        // Entrada
+
+        Console.Write("Dia ");
+        int dia_inicio = int.Parse(Console.ReadLine());
+        string[] I = Console.ReadLine().Split(':');
+
+        int horas_inicio = int.Parse(I[0]);
+        int minutos_inicio = int.Parse(I[1]);
+        int segundos_inicio = int.Parse(I[2]);
+
+        Console.Write("Dia ");
+        int dia_fim = int.Parse(Console.ReadLine());
+        string[] F = Console.ReadLine().Split(':');
+
+        int horas_fim = int.Parse(F[0]);
+        int minutos_fim = int.Parse(F[1]);
+        int segndos_fim = int.Parse(F[2]);
+
+        // Calculos
+
+        int dias_totais =
+        int horas_totis =
+        int minutos_totais = 
+        int segundos_totais = 
+
+        Console.WriteLine(dias_totais);
+
+
+
+
+    }
+
+}
