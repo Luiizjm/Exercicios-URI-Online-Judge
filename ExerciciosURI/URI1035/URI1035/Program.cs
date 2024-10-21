59cbfac331f7306f3ce01282bcf9b4c7e6bd1f1e
Luiizjm <luiizjm@outlook.com>
2024-10-21 17:22:30 -0300

    Att

---
+++ ExerciciosURI/URI1035/URI1035/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] n = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(n[0]);
+        int B = int.Parse(n[1]);
+        int C = int.Parse(n[2]);
+        int D = int.Parse(n[3]);
+
+        if (B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0)
+        {
+            Console.WriteLine("Valores aceitos");
+        }
+        else
+        {
+            Console.WriteLine("Valores nao aceitos");
+        }
+    }
+}
